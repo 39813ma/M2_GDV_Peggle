ae02d53c3d1a493ecd09f404e2703952fa1bea1b
Marnix <marnixvanderwerff@gmail.com>
2025-12-04 14:55:57 +0100

    new

---
+++ Assets/Scripts/TargetCollision.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class TargetCollision : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Hit!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
